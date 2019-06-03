67419f9e311e42857f5bdfcade77cdb68fab95a7
0trebor0 <test@test.com>
2019-06-03 14:35:15 +0100

    new files

---
+++ RTCPeerConnection-test/htdocs/script.js
@@ -0,0 +1,80 @@
+var localVideo = document.getElementById('localVideo');
+var remoteVideo = document.getElementById('remoteVideo');
+var startButton = document.getElementById('startButton');
+var stopButton = document.getElementById('stopButton');
+var WebsocketHistory = document.getElementById('WebsocketHistory');
+var websocket = new WebSocket("ws://192.168.137.1:8080");
+var localStream;
+var connectionPeer = new RTCPeerConnection({ "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }] });
+connectionPeer.onicecandidate = ( event )=>{
+	if( event.candidate ){
+		wSend( {"type":"candidate","candidate":event.candidate} );
+	}
+}
+connectionPeer.onaddstream = ( event )=>{
+	remoteVideo.srcObject = event.stream;
+	remoteVideo.play();
+}
+websocket.onmessage = ( event )=>{
+	WebsocketHistory.innerHTML += "<p style='margin:10px;background-color:lightblue;'>"+event.data+"</p>";
+	let array = JSON.parse( event.data );
+	console.log( array );
+	switch( array.type ){
+		case "offer":
+			startButton.disabled = true;
+			stopButton.disabled = false;
+			onOffer( array.offer );
+		break;
+		case "answer":
+			onAnswer( array.answer );
+		break;
+		case "candidate":
+
+		break
+	}
+	console.log( connectionPeer.getStats() );
+}
+navigator.getUserMedia({video: true}, gotSTream, ( err )=>{
+	console.log( err );
+});
+function start(){
+	startButton.disabled = true;
+	stopButton.disabled = false;
+	connectionPeer.createOffer( ( offer )=>{
+		wSend( {"type":"offer","offer":offer} );
+		connectionPeer.setLocalDescription( offer );
+	}, ( err )=>{
+		console.log( err );
+		document.getElementById('errorMsg').innerHTML = err;
+	} );
+}
+function stop(){
+	stopButton.disabled = true;
+	startButton.disabled = false;
+	localVideo.srcObject = null;
+}
+function gotSTream( stream ){
+	localStream = stream;
+	localVideo.srcObject = stream;
+	localVideo.play();
+	connectionPeer.addStream( localStream );
+}
+function wSend( message ){
+	websocket.send( JSON.stringify( message ) );
+}
+function onOffer( offer ){
+	connectionPeer.setRemoteDescription( offer );
+	connectionPeer.createAnswer( ( answer )=>{
+		connectionPeer.setLocalDescription( answer );
+		wSend( {"type":"answer","answer":answer} );
+	},( err )=>{
+		console.log( err );
+		document.getElementById('errorMsg').innerHTML = err;
+	} );
+}
+function onAnswer( answer ){
+	connectionPeer.setRemoteDescription( answer );
+}
+function onIcecandidate( candidate ){
+	connectionPeer.addIceCandidate( candidate );
+}
--- RTCPeerConnection-test/main.js
+++ RTCPeerConnection-test/main.js
@@ -1,1 +1,61 @@
-
+const http = require('http');
+const fs = require('fs');
+const mime = require('mime-types');
+const url = require('url');
+const WebSocket = require('ws');
+let port = 80;
+let htdocs = __dirname+'/htdocs/';
+try{
+    const server = http.createServer();
+    const Wss = new WebSocket.Server( {port:8080} );
+    server.listen(port);
+    server.on('listening', ()=>{
+        console.log("   Server Started on PORT:"+port);
+    });
+    server.on(  'request', ( req, res )=>{
+        let URL = url.parse( req.url, true );
+        let ipAddress = req.connection.remoteAddress;
+        console.log( "  "+ipAddress+" "+req.method+" "+req.url );
+        if( URL.pathname == '/' ){
+			if( fs.existsSync( htdocs+"index.html" ) ){
+				res.writeHead(200, {'Content-Type': mime.lookup( htdocs+URL.pathname )});
+				fs.createReadStream( htdocs+"index.html" ).pipe( res );
+			} else {
+				res.writeHead(404, {'Content-Type': 'text/html'});
+				res.write( "error 404" );
+				res.end();
+			}
+		}else if( fs.existsSync( htdocs+URL.pathname ) ){
+			res.writeHead(200, {'Content-Type': mime.lookup( htdocs+URL.pathname )});
+            fs.createReadStream( htdocs+URL.pathname ).pipe( res );
+        }else{
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write( "<head><title>404 not found</title></head><body><center><h1>404 Not Found</h1><p>"+URL.pathname+" not found</p></center></body>" );
+            res.end();
+        }
+    });
+    Wss.on( 'connection', ( connection )=>{
+		console.log( "new connection" );
+        connection.on( 'message', ( message )=>{
+            console.log( message );
+            if( isJson( message ) == true ){
+                let array = JSON.parse( message );
+				Wss.clients.forEach( ( client )=>{
+					if( client !== connection && client.readyState === WebSocket.OPEN ){
+						client.send( message );
+					}
+				} );
+            }
+        } );
+    } );
+}catch( error ){
+    console.log( error );
+}
+function isJson( message ){
+    try{
+        JSON.parse( message );
+    } catch( err ){
+        return false;
+    }
+    return true;
+}
