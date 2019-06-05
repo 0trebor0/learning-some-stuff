41c2dac62cef9bb745d7e2b04f50b5bb74ddb39f
0trebor0 <test@test.com>
2019-06-05 11:03:23 +0100

    Working Changes

--- RTCPeerConnection-test/htdocs/videoLiveStream/script.js
+++ RTCPeerConnection-test/htdocs/videoLiveStream/script.js
@@ -7,17 +7,8 @@ var videoPlayer = document.getElementById('videoPlayer');
 var clientPlayer = document.getElementById('clientPlayer');
 var WebsocketHistory = document.getElementById('WebsocketHistory');
 var websocket = new WebSocket("ws://192.168.137.1:8080");
-var videoStream;
-var connectionPeer = new RTCPeerConnection({ "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }] });
-connectionPeer.onicecandidate = ( event )=>{
-	if( event.candidate ){
-		wSend( {"type":"candidate","candidate":event.candidate} );
-	}
-}
-connectionPeer.onaddstream = ( event )=>{
-	clientPlayer.srcObject = event.stream;
-	clientPlayer.play();
-}
+var videoStream = null;
+var connectionPeer;
 websocket.onmessage = ( event )=>{
 	WebsocketHistory.innerHTML += "<p style='margin:10px;background-color:lightblue;'>"+event.data+"</p>";
 	let array = JSON.parse( event.data );
@@ -34,27 +25,50 @@ websocket.onmessage = ( event )=>{
 		case "candidate":
 			onIcecandidate( array.candidate );
 		break
+		case "leave":
+			startButton.disabled = false;
+			stopButton.disabled = true;
+			clientPlayer.srcObject = null;
+		break;
 	}
 }
 videoPlayer.oncanplay = ()=>{
 	videoStream = videoPlayer.captureStream();
+	connectionPeer = new RTCPeerConnection({ "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }] });
+	connectionPeer.onicecandidate = ( event )=>{
+		if( event.candidate ){
+			wSend( {"type":"candidate","candidate":event.candidate} );
+		}
+	}
+	connectionPeer.onaddstream = ( event )=>{
+		clientPlayer.srcObject = event.stream;
+		clientPlayer.oncanplay = ()=>{
+			clientPlayer.play();
+		}
+	}
 	connectionPeer.addStream( videoStream );
+}
+clientPlayer.oncanplay = ()=>{
+	clientPlayer.play();
 }
 start = ()=>{
 	startButton.disabled = true;
 	stopButton.disabled = false;
-	console.log( videoStream );
+	if( connectionPeer !== null ){
+		connectionPeer.createOffer( ( offer )=>{
+			wSend( {"type":"offer","offer":offer} );
+			connectionPeer.setLocalDescription( offer );
+		}, ( err )=>{
+			console.log( err );
+			document.getElementById('errorMsg').innerHTML = err;
+		} );
+	}
 }
 stop = ()=>{
 	startButton.disabled = false;
 	stopButton.disabled = true;
-	connectionPeer.createOffer( ( offer )=>{
-		wSend( {"type":"offer","offer":offer} );
-		connectionPeer.setLocalDescription( offer );
-	}, ( err )=>{
-		console.log( err );
-		document.getElementById('errorMsg').innerHTML = err;
-	} );
+	clientPlayer.srcObject = null;
+	wSend( {"type":"leave"} );
 }
 function onIcecandidate( candidate ){
 	connectionPeer.addIceCandidate( new RTCIceCandidate( candidate ) );
